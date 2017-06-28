135e87aa01f9cd52f1c4c33802fa0f2fc2cfe9e1
Oliver <oliver@nemos-MBP.fritz.box>
2017-06-28 22:08:00 +0200

    Model, Make, DbContext

---
+++ Persistence/VegaDBContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Vega.Persistence
+{
+    public class VegaDbContext : DbContext
+    {
+        public VegaDbContext(DbContextOptions<VegaDbContext> options)
+        :base(options)
+        {
+            
+        }
+        
+    }
+}
